0bd93f39681f8c8509ce1e29e922dc14384e58aa
juanmachabaudie <jumachabaudie@gmail.com>
2021-07-05 16:40:11 -0300

    Product & Category Models done

---
+++ api/src/models/Category.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require("sequelize");
+
+module.exports = (sequelize) => {
+  sequelize.define(
+    "category",
+    {
+      uuid: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.ARRAY(DataTypes.STRING),
+      },
+      clicks: {
+        type: DataTypes.INTEGER,
+        defaultValue: 0,
+      },
+    },
+    { timestamps: false }
+  );
+};
