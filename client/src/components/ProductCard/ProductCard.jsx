ed3f8541255488d4654f4806c5cd78460abb9ad5
juanmachabaudie <jumachabaudie@gmail.com>
2021-07-06 20:16:19 -0300

    Product Card To Do

---
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import CardMedia from '@material-ui/core/CardMedia'
+
+const ProductCard = () => {
+  return (
+    <div>
+      <CardMedia
+        title=""
+        image=
+      />
+    </div>
+  )
+}
+
+export default ProductC
