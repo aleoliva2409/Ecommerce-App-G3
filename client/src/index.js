3c7abd7bddb95e4f0ada7b892bd4213d14a45534
aleoliva2409 <alejandro.oliva2409@gmail.com>
2021-07-06 20:56:24 -0300

    adding dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,23 @@
+import "@fontsource/roboto";
 import { CssBaseline } from "@material-ui/core";
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
-import store from './redux/store';
-import { BrowserRouter } from 'react-router-dom'
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
-	<React.StrictMode>
-		<Provider store={store}>
-			<BrowserRouter>
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
         <CssBaseline />
-				<App />
-			</BrowserRouter>
-		</Provider>
-	</React.StrictMode>,
-	document.getElementById("root")
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
