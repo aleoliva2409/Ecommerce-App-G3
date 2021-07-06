5b28520bbafdb8a663c0b493cd927b8fd41f25bd
kevin <kevin.queiro@outlook.com>
2021-07-06 00:22:26 -0300

    s18 and s19 done
    .../api/categories/delete (delete)
    .../api/categories/add (add)

--- api/src/controllers/category.controller.js
+++ api/src/controllers/category.controller.js
@@ -1,47 +1,46 @@
 const { Category } = require("../db");
 
 const deleteCategory = async (req, res) => {
-  const { id } = req.body;
-  try {
+    const { id } = req.body;
+    try {
         const remove = await Category.findOne({
             where: {
                 id
             },
         });
-     if(remove)
-     {
-        Category.destroy({
-            where:{
-                id
-            },
-         })
-         res.status(200).json({message:"Category delete"});
-  } else{
-    res.status(404).send({message:"Category not found"});
-  }
-}
-  catch (err) {
-    res.status(404)
-  }
+        if (remove) {
+            Category.destroy({
+                where: {
+                    id
+                },
+            })
+            res.status(200).json({ message: "Category delete" });
+        } else {
+            res.status(404).json({ message: "Category not found" });
+        }
+    }
+    catch (err) {
+        console.log(err)
+    }
 };
 
 
-const addCategory = async (req,res,next) => {
+const addCategory = async (req, res, next) => {
     try {
         const { name, description } = req.body
-        if(name){
-            const find = await Category.findOne({ where: {name} })
-            if (find){
+        if (name) {
+            const find = await Category.findOne({ where: { name } })
+            if (find) {
                 return res.status(500).json({ error: 'this category alredy exists' })
             } else {
                 const category = await Category.create(req.body)
                 return res.status(200).json({ success: `'${category.name}' category added `, category })
             }
         } else { return res.status(500).json({ error: 'category data is missing' }) }
-    } catch (error){ next(error) }
+    } catch (error) { next(error) }
 }
 
 module.exports = {
     addCategory,
-  deleteCategory
+    deleteCategory
 }
