1b79d13db2629261186ee12a10f85c112c33d1ef
aleoliva2409 <alejandro.oliva2409@gmail.com>
2021-07-07 15:04:03 -0300

    fix public

--- client/src/components/ProductCard/Catalogue.jsx
+++ client/src/components/ProductCard/Catalogue.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import ProductCard from './ProductCard'
+import Typography from '@material-ui/core/Typography'
 
 
 const Catalogue = () => {
   return (
-    <Grid container spacing={1}>
-      <Grid item xs={10}>
-        <ProductCard />
+    <div>
+      <Typography variant="h1" color="initial">Products</Typography>
+      <Grid container spacing={1}>
+        <Grid item xs={10}>
+          <ProductCard />
+        </Grid>
       </Grid>
-    </Grid>
+    </div>
   )
 }
 
