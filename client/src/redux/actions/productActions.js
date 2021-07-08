d02944803ef32165074569ac6355d258e50ba11c
aleoliva2409 <alejandro.oliva2409@gmail.com>
2021-07-08 20:11:22 -0300

    change actions

--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios from "axios";
 
 export const GET_PRODUCT_DETAILS = "GET_PRODUCT_DETAILS";
 export const GET_PRODUCTS = "GET_PRODUCTS";
@@ -7,14 +7,14 @@ export const ACTIVE_LOADING = "ACTIVE_LOADING";
 // * Set data of each product
 
 export const getProductDetails = (id) => async (dispatch) => {
-	try {
-        const { data } = await axios.get(`/products/${id}`);
-        dispatch({
-          type: GET_PRODUCT_DETAILS,
-          payload: data,
-        })
-  }catch(error){
-    console.log(error)
+  try {
+    const { data } = await axios.get(`/products/${id}`);
+    dispatch({
+      type: GET_PRODUCT_DETAILS,
+      payload: data,
+    });
+  } catch (error) {
+    console.log(error);
   }
 };
 
@@ -24,17 +24,17 @@ export const getProducts = () => async (dispatch) => {
     dispatch({
       type: GET_PRODUCTS,
       payload: data,
-    })
-  }catch(error){
-    console.log(error)
+    });
+  } catch (error) {
+    console.log(error);
   }
-}
+};
 
 // * SetLoading on true to display the loading component
 
 export const activeLoading = () => {
-  return({
+  return {
     type: ACTIVE_LOADING,
     isLoading: true,
-  })
-}
+  };
+};
