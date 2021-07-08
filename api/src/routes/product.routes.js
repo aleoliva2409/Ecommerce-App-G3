f3a9fc89b1a220a61a24c78699676f6932ecc357
Agustina <68576630+Agustina-vardaro@users.noreply.github.com>
2021-07-08 20:54:18 -0300

    corrections and s22

--- api/src/routes/product.routes.js
+++ api/src/routes/product.routes.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
-const { getProducts, getProductsAll, getById, addProduct, updateProduct, deleteProduct } = require("../controllers/product.controller");
+const { getProducts, getProductsAll, getById, addProduct, updateProduct, deleteProduct, getProductsByCategory} = require("../controllers/product.controller");
 
 
 const router = Router();
 
 router.get("/", getProducts);//! commented to review
+router.get("/category/:id", getProductsByCategory);
 router.get("/all", getProductsAll);
 router.get("/:id", getById)
 router.post("/", addProduct)
