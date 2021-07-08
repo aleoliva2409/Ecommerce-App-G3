2dc783340667b39ee58b3b1a90251c93793fc586
aleoliva2409 <alejandro.oliva2409@gmail.com>
2021-07-08 18:48:20 -0300

    little fixies

--- client/src/components/Products/Catalogue.jsx
+++ client/src/components/Products/Catalogue.jsx
@@ -7,7 +7,7 @@ const Catalogue = ({products}) => {
   console.log(products)
   return (
     <Box p={3}>
-      <Grid container spacing={6} justifyContent="center" >
+      <Grid container spacing={6} >
         {products &&
           products.map((product) => (
             <Grid item xl={3} lg={4} md={6} sm={6} xs={12} key={product.id}>
