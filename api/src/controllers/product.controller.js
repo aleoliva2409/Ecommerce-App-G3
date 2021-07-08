ac2527f54c3347f005336ff49fecea2389f1635c
Agustina <68576630+Agustina-vardaro@users.noreply.github.com>
2021-07-08 11:28:18 -0300

    returns all products with their categories

--- api/src/controllers/product.controller.js
+++ api/src/controllers/product.controller.js
@@ -1,17 +1,36 @@
 const { Product, Category } = require("../db");
+const {productCategory} = require("../helpers/utils");
 const Op = require('sequelize').Op;
 
 
-//?may have to change this route
-const getProductsAll = async (req, res, next) => {
+const getProductsAll = async (req, res,next) => {
   try {
-    const products = await Product.findAll();
-    if (products.length) return res.send(products)
-    else return res.status(404).json({ error: "Product not found" })
-  } catch (err) {
-    next(err)
+    const dbProducts = await Product.findAll();
+    arrProducts =[];
+    if (dbProducts.length) {
+      for (element of dbProducts) {
+        const values = element.dataValues;
+        let arrCategories = await productCategory(values.id);
+        const objProduct = {
+          id: values.id,
+          name: values.name,
+          description: values.description,
+          image: values.image,
+          price: values.price,
+          stock: values.stock,
+          size: values.size,
+          categories: arrCategories,
+        };
+        arrProducts.push(objProduct);
+      }
+    } else {
+      res.status(404).json({ message: "Empty database" });
+    }
+    res.status(200).json(arrProducts);
+  } catch (error) {
+    next(error);
   }
-};
+}
 
 //! this route return the products with the "name" receibed, to review
 const getProducts = async (req, res, next) => {
