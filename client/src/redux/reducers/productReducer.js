530af79ea47a59aac201a81811a750ee4d8e2799
aleoliva2409 <alejandro.oliva2409@gmail.com>
2021-07-08 21:15:34 -0300

    SearchPage

--- client/src/redux/reducers/productReducer.js
+++ client/src/redux/reducers/productReducer.js
@@ -1,31 +1,30 @@
 import {
-    GET_PRODUCT_DETAILS,
-    ACTIVE_LOADING,
-    GET_PRODUCTS,
-    SEARCH_PRODUCTS
-  } from "../actions/productActions";
+  GET_PRODUCT_DETAILS,
+  ACTIVE_LOADING,
+  GET_PRODUCTS,
+  SEARCH_PRODUCTS,
+} from "../actions/productActions";
 
 const initialState = {
   allProducts: [],
   searchProducts: [],
   product: {},
   isLoading: true,
+};
 
-}
-
-const productReducer = (state=initialState, action) => {
-  switch(action.type){
+const productReducer = (state = initialState, action) => {
+  switch (action.type) {
     case SEARCH_PRODUCTS:
-      return {...state, searchProducts: action.search}; //* Array with search products
+      return { ...state, searchProducts: action.search }; //* Array with search products
     case GET_PRODUCTS:
-      return {...state, allProducts: action.allProducts}; //* Array with all products
+      return { ...state, allProducts: action.payload }; //* Array with all products
     case GET_PRODUCT_DETAILS:
-      return {...state, product: action.product, isLoading: false}; //* Especific product data
+      return { ...state, product: action.payload, isLoading: false }; //* Especific product data
     case ACTIVE_LOADING:
-      return {...state, isLoading:action.isLoading}; //* just set the loading variable
+      return { ...state, isLoading: action.payload }; //* just set the loading variable
     default:
       return state;
   }
-}
+};
 
 export default productReducer;
