3db926d65e5bb3aaacd4b7a8dbd3ae8f874f74fa
aleoliva2409 <alejandro.oliva2409@gmail.com>
2021-07-13 15:28:42 -0300

    searchpage

--- client/src/views/SearchPage.jsx
+++ client/src/views/SearchPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import FilterProducts from "../components/Products/FilterProducts/FilterProducts";
 import Catalogue from "../components/Products/Catalogue";
@@ -8,6 +8,13 @@ import { getSearchProducts } from "../redux/actions/productActions";
 const SearchPage = () => {
   const dispatch = useDispatch();
   const searchProducts = useSelector((state) => state.products.searchProducts);
+  const filterproducts = useSelector((state) => state.products.filterproducts.products);
+
+  let render
+  const [filter,setFilter] = useState(true);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  filter ? render = searchProducts : render = filterproducts || [];
 
   useEffect(() => {
     dispatch(getSearchProducts());
@@ -20,10 +27,10 @@ const SearchPage = () => {
       </Typography>
       <Grid container spacing={2}>
         <Grid item xl={2} lg={2} md={3} sm={12} xs={12}>
-          <FilterProducts />
+          <FilterProducts setFilter={setFilter} setCurrentPage={setCurrentPage}/>
         </Grid>
         <Grid item xl={10} lg={10} md={9} sm={12} xs={12}>
-          <Catalogue products={searchProducts} />
+          <Catalogue products={render} currentPage={currentPage} setCurrentPage={setCurrentPage}/>
         </Grid>
       </Grid>
     </Container>
