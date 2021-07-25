c2d9110dc855998eed475de8db2f64099f6ef839
sggrenas2 <sgarcias667@gmail.com>
2021-07-25 18:53:17 -0300

    removing testing component on home page

--- client/src/views/HomePage.jsx
+++ client/src/views/HomePage.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Container } from "@material-ui/core";
 import Carrousel from '../components/Home/Carrousel/Carrousel';
-import Table from '../components/OrdersTable/OrderTable.jsx';
 
 
 const HomePage = () => {
   return (
     <Container maxWidth="xl">
       <Carrousel />
-      <Table />
     </Container>
   )
 }
