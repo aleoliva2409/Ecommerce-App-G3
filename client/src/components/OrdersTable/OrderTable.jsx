42b5dd6967c9b4935864f1ff6116c1bdbadf8dae
sggrenas2 <sgarcias667@gmail.com>
2021-07-25 14:34:01 -0300

    update

--- client/src/components/OrdersTable/OrderTable.jsx
+++ client/src/components/OrdersTable/OrderTable.jsx
@@ -42,8 +42,8 @@ const RowsTable = ({order}) => {
 
   const getTotal = () => {
     let total = 0;
-    for(let i=0; i < order.cart.length; i++){
-      total += order.cart[i].price;
+    for(let i=0; i < order.products.length; i++){
+      total += order.products[i].orderlines.price;
     }
     return total;
   }
@@ -180,112 +180,7 @@ const OrdersTable = () => {
 
   const classes = useStyles();
   const dispatch = useDispatch();
-  const orders = useSelector(state => state.orders.ordersList).concat([
-    {
-      orderState:"processing",
-      shippingState:"initial",
-      shippingCost:600.99,
-      shippingAddres:"juan b justo 400",
-      shippingZip:"1508",
-      shippingCity:"buenos Aires",
-      comments:"marcarme al celular",
-      paymentDetails:"debit",
-      firstName:"camila",
-      lastName:"bonilla",
-      cart:[
-        {
-          id: 1,
-          name: "Colchón y Sommier Suavegom Merit 80x190",
-          cant: 2,
-          price: 35800.00
-        }
-      ],
-    },
-    {
-      orderState:"cancelled",
-      shippingState:"created",
-      shippingCost:600.99,
-      shippingAddres:"condarco 800",
-      shippingZip:"1508",
-      shippingCity:"buenos Aires",
-      comments:"dejar en porteria",
-      paymentDetails:"credit",
-      firstName:"felipe",
-      lastName:"alvarez",
-      cart:[
-        {
-          id: 1,
-          name: "Colchón y Sommier Suavegom Merit 80x190",
-          cant: 2,
-          price: 35800.00
-        },
-        {
-          id: 2,
-          name: "Colchón y Sommier King Koil Brighton 200x200",
-          cant: 1,
-          price: 158100.00
-        },
-        {
-          id: 3,
-          name: "Colchón y Sommier Elegante Señorial 180x200",
-          cant: 1,
-          price: 86000.00
-        }
-      ],
-    },
-    {
-      orderState:"pending",
-      shippingState:"processing",
-      shippingCost:600.99,
-      shippingAddres:"juan b alberdi 200",
-      shippingZip:"1508",
-      shippingCity:"buenos Aires",
-      comments:"golpear fuerte la puerta",
-      paymentDetails:"debit",
-      firstName:"camilo",
-      lastName:"moralez",
-      cart:[
-        {
-          id: 1,
-          name: "Colchón y Sommier Suavegom Merit 80x190",
-          cant: 2,
-          price: 35800.00
-        },
-        {
-          id: 3,
-          name: "Colchón y Sommier Elegante Señorial 180x200",
-          cant: 1,
-          price: 86000.00
-        }
-      ],
-    },
-    {
-      orderState:"completed",
-      shippingState:"cancelled",
-      shippingCost:600.99,
-      shippingAddres:"curapaligue 1000",
-      shippingZip:"1508",
-      shippingCity:"buenos Aires",
-      comments:"dejar en la puerta lateral",
-      paymentDetails:"credit",
-      firstName:"jaime",
-      lastName:"angulo",
-      cart:[
-        {
-          id: 2,
-          name: "Colchón y Sommier King Koil Brighton 200x200",
-          cant: 1,
-          price: 158100.00
-        },
-        {
-          id: 3,
-          name: "Colchón y Sommier Elegante Señorial 180x200",
-          cant: 1,
-          price: 86000.00
-        }
-      ],
-    },
-  ]);
+  const orders = useSelector(state => state.orders.ordersList);
   const [page, setPage] = useState(0);
   const [rowsPerPage, setRowsPerPage] = useState(5);
   const [columnActiveOrder, setColumnActiveOrder] = useState(0);
