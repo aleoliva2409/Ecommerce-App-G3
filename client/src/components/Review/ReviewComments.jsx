ac1f97cd57a386559de2b4769f0624c9a80ee99c
Facundo Vergara <fakumax@gmail.com>
2021-07-29 11:10:35 -0300

    Fix Review

---
+++ client/src/components/Review/ReviewComments.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Rating from '@material-ui/lab/Rating';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+function ReviewComments() {
+  const [value, setValue] = React.useState(2);
+  return (
+    <div>
+        <Box component="fieldset" mb={3} borderColor="transparent">
+          <Typography component="legend">Read only</Typography>
+          <Rating name="read-only" value={value} readOnly />
+        </Box>
+
+    </div>
+  )
+}
+
+export default ReviewComments;
+
--- client/src/views/LoginPage.jsx
+++ client/src/views/LoginPage.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import Review from '../components/Review/Review';
+import ReviewComments from '../components/Review/ReviewComments';
+import { Grid } from '@material-ui/core';
+// * STYLES *
+//import { useStyles } from './ProductStyle.js';
 
 const LoginPage = () => {
+  //const classes = useStyles();
   return (
-    <div>
-      <h1>LoginPage</h1>
-    </div>
+    <Grid container spacing={2}  >
+        <Grid item md={6} direction='column'>
+            <Review />
+        </Grid>
+        <Grid item md={6} direction='column'>
+            <ReviewComments />
+        </Grid>
+    </Grid>
   )
 }
 
