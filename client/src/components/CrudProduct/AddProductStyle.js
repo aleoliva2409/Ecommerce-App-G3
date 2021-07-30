f167ade1ddc07c3bdcaabc3592280a877b0451a7
juanmachabaudie <jumachabaudie@gmail.com>
2021-07-30 03:45:47 -0300

    Add & Edit Forms

--- client/src/components/CrudProduct/AddProductStyle.js
+++ client/src/components/CrudProduct/AddProductStyle.js
@@ -1,21 +1,25 @@
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles(theme => ({
-  // formAddProduct:{
-  //   width:300,
-  // },
-  buttonAddProduct:{
-    color:'#232020',
-    border: '2px solid',
-    borderColor: '#232020',
-    '&:hover':{
-      color:'#00BBC9',
-      borderColor: '#00BBC9'
-    }
+const useStyles = makeStyles(({ spacing, breakpoints }) => ({
+
+  formAddProduct: {
+    width: "25rem",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
   },
-  textField: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1),
+  field: {
+    width: "100%",
+    margin: spacing(1)
+  },
+  measures: {
+    display: "flex",
+    justifyContent: "space-around",
+    alignItems: "center"
+  },
+  measuresField: {
+    width: "30%"
   }
 }))
 
